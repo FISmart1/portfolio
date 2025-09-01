c0f5492101eba06204934375bc8d39d8206a1daa
FISmart1 <yusufftibazma@gmail.com>
2025-09-01 08:25:08 +0700

    ganti bagian skill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -138,21 +138,14 @@ export default function Page() {
             <h2 className="text-xl font-bold">Skills</h2>
           </BlurFade>
 
-          <Marquee pauseOnHover>
-            {DATA.skills.map((skill) => (
-              <Badge key={skill} className="rounded px-4 py-1 text-sm">
-                {skill}
-              </Badge>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
             ))}
-          </Marquee>
+          </div>
 
-          <Marquee reverse pauseOnHover>
-            {DATA.skills.map((skill) => (
-              <Badge key={skill} className="rounded px-4 py-1 text-sm">
-                {skill}
-              </Badge>
-            ))}
-          </Marquee>
 
         </div>
       </section>
