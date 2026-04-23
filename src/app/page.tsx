ad59f2498d095adaa25fccd4207ac08bd306927d
FISmart1 <yusufftibazma@gmail.com>
2026-04-23 20:46:08 +0700

    fix in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -218,7 +218,6 @@ export default function Page() {
                     location={project.location}
                     dates={project.dates}
                     image={project.image}
-                    links={project.links}
                   />
                 </BlurFade>
               ))}
