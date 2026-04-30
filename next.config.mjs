7864f152f38ad005ce8af8b1a246ac60fb55447c
FISmart1 <yusufftibazma@gmail.com>
2026-05-01 05:32:46 +0700

    tambahin di next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+// next.config.js - Tambahkan domain eksternal
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+    ],
+  },
+}
+
 export default nextConfig;
